cd0d04a64fd0d3c1739c297ce3e2b66ddc484794
Pizzacoder6721 <Pizzalover250@hotmail.com>
2025-10-04 17:49:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+//Puff Sprite
+const puff = document.getElementById('puffs');
+//movement
+let puffX = 0;
+let puffY = 0;
+const puffSpeed = 5;
+//update puff pos
+function updatePuffPos(){
+  sprite.style.left = `${x}px`;
+  sprite.style.top = `${y}px`;
+} 
 
+//Annoying event listener
+document.addEventListener("keyDown", (event) => {
+  switch (event.key){
+    case "ArrowUp":
+    case "w":
+      y -= speed;
+      break;
+    
+    case "ArrowDown":
+    case "s":
+      y += speed;
+      break;
+
+    case "ArrowLeft":
+    case "a":
+      x -= speed;
+      break;
+
+    case "ArrowRight":
+    case "d":
+      x += speed;
+      break;
+  }
+  updatePuffPos();
+});
+updatePuffPos();
