52ff7fe94ff76e6a1cc57088cd0b751878d24d8a
Pizzacoder6721 <Pizzalover250@hotmail.com>
2025-10-18 10:58:37 -0400

    Increase jump force and gravity values

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const puff = document.getElementById('puffs');
 let puffX = 0;
 let puffY = 0;
 const puffSpeed = 0.5;
-let jumpForce = -0.7;
+let jumpForce = -3;
 
 const keysPressed = {};
 
@@ -59,7 +59,7 @@ function gameLoop() {
     }
 
     if (!onGround){
-        gravity = 0.7
+        gravity = 3;
     }
 
     puffVelocityX *= 0.95;
